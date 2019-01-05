e2068d7c99759c10a04a0ae1ecb6b037df9aa90e
wuh06 <wuh06@mingyuanyun.com>
2019-01-05 17:48:43 +0800

    add nuget info

--- AssemblyHasher.Console/Program.cs
+++ AssemblyHasher.Console/Program.cs
@@ -7,13 +7,23 @@ namespace AssemblyHasher.Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            //System.Console.WriteLine("Hello World!");
 
-            var path = @"D:\workspace\rdc\AssemblyComparer\AssemblyComparer.Core\bin\Debug\netstandard2.0\AssemblyComparer.Core.dll";
+            var path = @"E:\FileClientAPI\2\myDoc.ApiClient.dll";
 
             var factory = HasherFactory.CreateHasher(path);
 
             var md5 = factory.GetHash(path);
+
+            System.Console.WriteLine(md5);
+
+            path = @"E:\myDoc.ApiClient.dll";
+
+            factory = HasherFactory.CreateHasher(path);
+
+            md5 = factory.GetHash(path);
+
+            System.Console.WriteLine(md5);
 
             System.Console.ReadKey();
         }
